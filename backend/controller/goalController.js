32ee6b9429ea10daa018c730873bad6332d75fba
Marcos <m.rodrig.felix@gmail.com>
2022-03-20 21:57:50 -0700

    Create congif/db.js and add import mongoose

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -4,14 +4,14 @@ const asyncHandler = require('express-async-handler')
 // @descGet Get goals
 // @routes GET /api/goals
 // @access Private
-const getGoals = asynchHandler( async (req, res) => {
+const getGoals = asyncHandler( async (req, res) => {
   res.status(200).json({message: `Get goals`})
 })
 
 // @descGet Set goal
 // @routes POST /api/goals
 // @access Private
-const setGoal = asynchHandler( async (req, res) => {
+const setGoal = asyncHandler( async (req, res) => {
   if(!req.body.text){
     res.status(400)
     throw new Error('Please add some text')
@@ -23,14 +23,14 @@ const setGoal = asynchHandler( async (req, res) => {
 // @descGet Update goal
 // @routes PUT /api/goals/:id
 // @access Private
-const updateGoal = asynchHandler( async (req, res) => {
+const updateGoal = asyncHandler( async (req, res) => {
   res.status(200).json({message: `Update goal ${req.params.id} `})
 })
 
 // @descGet Delete goals
 // @routes DELETE /api/goals/:id
 // @access Private
-const deleteGoal = asynchHandler( async (req, res) => {
+const deleteGoal = asyncHandler( async (req, res) => {
   res.status(200).json({message: `Delete goal ${req.params.id} `})
 })
 
