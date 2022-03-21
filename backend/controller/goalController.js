09a8c653ecda95b9c60c23409b0c7f0479525e00
Marcos <m.rodrig.felix@gmail.com>
2022-03-20 23:42:24 -0700

    Can update Goal and delete Goal, full REST API set for goals

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -21,7 +21,7 @@ const setGoal = asyncHandler( async (req, res) => {
   }
 
   const goal = await Goal.create({ // CREATE NEW GOAL
-    text: req.body.text
+    text: req.body.text,
   })
 
   res.status(200).json(goal)
@@ -31,14 +31,36 @@ const setGoal = asyncHandler( async (req, res) => {
 // @routes PUT /api/goals/:id
 // @access Private
 const updateGoal = asyncHandler( async (req, res) => {
-  res.status(200).json({message: `Update goal ${req.params.id} `})
+  const goal = await Goal.findById(req.params.id)
+
+  if (!goal){
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, 
+    {
+      new: true,
+    }
+  )
+
+  res.status(200).json(updatedGoal)
 })
 
 // @descGet Delete goals
 // @routes DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler( async (req, res) => {
-  res.status(200).json({message: `Delete goal ${req.params.id} `})
+  const goal = await Goal.findById(req.params.id)
+
+  if (!goal){
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  await goal.remove()
+
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
